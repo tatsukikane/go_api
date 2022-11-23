08069b9db29fcbe528b7ff488f86b004a334a234
tatsukikane <15210124kt@gmail.com>
2022-11-23 12:48:56 +0900

    fix bug

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -17,7 +17,6 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) 
 	todo := &model.Todo{
 		Text:   input.Text,
 		ID:     fmt.Sprintf("T%d", rand.Int()),
-		User:   &model.User{ID: input.UserID, Name: "user " + input.UserID},
 		UserID: input.UserID,
 	}
 	r.todos = append(r.todos, todo)
@@ -31,7 +30,8 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 
 // User is the resolver for the user field.
 func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
-	panic(fmt.Errorf("not implemented: User - user"))
+	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
+
 }
 
 // Mutation returns generated.MutationResolver implementation.
